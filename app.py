19415116550fc31dddc977cf9702890006d11910
pemula-newbie <102136876+pemula-newbie@users.noreply.github.com>
2023-03-24 14:59:07 +0700

    Update app.py sehingga bisa run di Google Colab

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 '''
 	Contoh Deloyment untuk Domain Data Science (DS)
-	Orbit Future Academy - AI Mastery - KM Batch 3
+	Orbit Future Academy - AI Mastery - KM Batch 4
 	Tim Deployment
-	2022
+	2023
 '''
 
 # =[Modules dan Packages]========================
 
 from flask import Flask,render_template,request,jsonify
+from flask_ngrok import run_with_ngrok
 import pandas as pd
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
@@ -17,6 +18,8 @@ from joblib import load
 
 app   = Flask(__name__, static_url_path='/static')
 model = None
+
+run_with_ngrok(app)
 
 # =[Routing]=====================================
 
@@ -72,8 +75,8 @@ if __name__ == '__main__':
 	# Load model yang telah ditraining
 	model = load('model_iris_dt.model')
 
-	# Run Flask di localhost 
-	app.run(host="localhost", port=5000, debug=True)
+	# Run Flask di Google Colab menggunakan ngrok
+	app.run()
 	
 	
 
